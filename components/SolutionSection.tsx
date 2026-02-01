c89d0fa02e8354bf3d8c0bf1fb0223b52a94bada
akshatidfy-pm <akshat.idfy@gmail.com>
2026-02-01 23:00:55 +0530

    Update SolutionSection.tsx
    Fixed delay time -> 0.1

--- components/SolutionSection.tsx
+++ components/SolutionSection.tsx
@@ -111,7 +111,7 @@ export const SolutionSection: React.FC = () => {
                 initial={{ opacity: 0, scale: 0.8 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 whileHover={{ scale: 1.15 }}
-                transition={{ duration: 0.3, delay: index * 0.1 }}
+                transition={{ duration: 0.3, delay: 0.1 }}
                 viewport={{ once: true }}
               >
                 {/* Icon Circle */}
